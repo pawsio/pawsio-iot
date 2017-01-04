c5af900df5977579e07a885569501003a84cf423
Christopher Bruner <chrbruner9@gmail.com>
2017-01-03 16:05:08 -0800

    abstracted out each sensor so that get data is one promise and all data will come back at the same time

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ const userApi = require('./lib/api/users-api');
 const petsApi = require('./lib/api/pets-api');
 const petSnapShotApi = require('./lib/api/pet-snapshots-api');
 const getData = require('./lib/get-data')();
+const rotary = require('./lib/rotary')();
 
 // TODO: I think for our purposes we assume user already signed up with our service
 
@@ -29,9 +30,14 @@ const user = require('./lib/setup/userinfo');
 const pet = require('./lib/setup/petinfo');
 let token = '';
 let petId = '';
+let dataPayload = [];
 
 function main() {
-    // first check token
+    // if rotary is negative and no payload, short-circuit
+    // if rotary is negative but have payload, check later whether we push
+    if(rotary() < 0 || dataPayload.length === 0) return;
+
+    // check token
     if(!token) {
         return userApi
                     .signin(user)
@@ -54,40 +60,29 @@ function main() {
     // collect data and post to page
     getData()
         .then(payload => {
-            payload.name = pet.name;
-            return petSnapShotApi.post(token, petId, payload);
+            console.log('from main', payload);
+            // will need to do the check here
+            // payload.name = pet.name;
+            // return petSnapShotApi.post(token, petId, payload);
         })
-        .then(res => {
-            console.log(res);
-            console.log('upload good!');
-        })
+        // .then(res => {
+        //     console.log(res);
+        //     console.log('upload good!');
+        // })
         .catch(err => console.error(err));
 };
 
-//setInterval(main, 7500);
+setInterval(main, 500);
 
-// var digitalAccelerometer = require('jsupm_mma7660');
-// // Instantiate an MMA7660 on I2C bus 0
-// var myDigitalAccelerometer = new digitalAccelerometer.MMA7660(
-//                     digitalAccelerometer.MMA7660_I2C_BUS,
-//                     digitalAccelerometer.MMA7660_DEFAULT_I2C_ADDR);
-// // place device in standby mode so we can write registers
-// myDigitalAccelerometer.setModeStandby();
 
-// // enable 64 samples per second
-// myDigitalAccelerometer.setSampleRate(digitalAccelerometer.MMA7660.AUTOSLEEP_64);
 
-// // place device into active mode
-// myDigitalAccelerometer.setModeActive();
+
 
 // var x, y, z;
 // x = digitalAccelerometer.new_intp();
 // y = digitalAccelerometer.new_intp();
 // z = digitalAccelerometer.new_intp();
-// var ax, ay, az;
-// ax = digitalAccelerometer.new_floatp();
-// ay = digitalAccelerometer.new_floatp();
-// az = digitalAccelerometer.new_floatp();
+
 
 // var outputStr;
 
@@ -105,52 +100,15 @@ function main() {
 //     console.log(outputStr);
 // }, 500);
 // // round off output to match C example, which has 6 decimal places
-// function roundNum(num, decimalPlaces) {
-//     var extraNum = (1 / (Math.pow(10, decimalPlaces) * 1000));
-//     return (Math.round((num + extraNum)
-//         * (Math.pow(10, decimalPlaces))) / Math.pow(10, decimalPlaces));
-// };
-// // When exiting: clear interval and print message
-// process.on('SIGINT', function() {
-//     clearInterval(myInterval);
-//     // clean up memory
-//     digitalAccelerometer.delete_intp(x);
-//     digitalAccelerometer.delete_intp(y);
-//     digitalAccelerometer.delete_intp(z);
-//     digitalAccelerometer.delete_floatp(ax);
-//     digitalAccelerometer.delete_floatp(ay);
-//     digitalAccelerometer.delete_floatp(az);
-//     myDigitalAccelerometer.setModeStandby();
-//     console.log("Exiting...");
-//     process.exit(0);
-// });
 
-var upm_grove = require('jsupm_grove');
-var groveRotary = new upm_grove.GroveRotary(1);
 
-loop();
 
-function loop() {
-    var abs = groveRotary.abs_value();
-    var absdeg = groveRotary.abs_deg();
-    var absrad = groveRotary.abs_rad();
-
-    var rel = groveRotary.rel_value();
-    var reldeg = groveRotary.rel_deg();
-    var relrad = groveRotary.rel_rad();
-
-    console.log('Abs: ' + abs + ' ' + Math.round(parseInt(absdeg)) + ' ' + absrad.toFixed(3));
-    console.log('Rel: ' + rel + ' ' + Math.round(parseInt(reldeg)) + ' ' + relrad.toFixed(3));
-
-//    setTimeout(loop, 1000);
-}
-
-var LCD = require('jsupm_i2clcd');
-var myLcd = new LCD.Jhd1313m1(3, 0x3E, 0x62);
+// var LCD = require('jsupm_i2clcd');
+// var myLcd = new LCD.Jhd1313m1(3, 0x3E, 0x62);
 
 //myLcd.setCursor(0,0);
 
-myLcd.setColor(107, 220, 247);
+// myLcd.setColor(107, 220, 247);
 //myLcd.write('Hello from PawsIO!');
 //myLcd.setCursor();
 
@@ -164,8 +122,8 @@ myLcd.setColor(107, 220, 247);
 //        i = -2;
 //    }
 //}
-myLcd.setCursor(0,0);
-myLcd.write('Hello from PawsIO!');
-myLcd.write('ello from PawsIO!');
+// myLcd.setCursor(0,0);
+// myLcd.write('Hello from PawsIO!');
+// myLcd.write('ello from PawsIO!');
 
 //setInterval(displayChange, 1000);
